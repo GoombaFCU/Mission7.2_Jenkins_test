1a0a0a90245b51cca9a5d86f12917f1d3a1c3c8b
GoombaFCU <a570405000@gmail.com>
2021-05-29 02:08:24 +0800

    Try to let coverage down again

--- app/src/test/java/Mission_7_2/PoolTest.java
+++ app/src/test/java/Mission_7_2/PoolTest.java
@@ -36,33 +36,6 @@ class PoolTest {
 		Exception e = assertThrows(Exception.class, ()->P.Cash_check("Normal", "Mon", 2, 4));
 		assertEquals("The swimming pool is close now.", e.getMessage());
 	}
-	/***
-	@Test
-	void Test_08_For_Coverage() throws Exception {
-		Exception e = assertThrows(Exception.class, ()->P.Cash_check("Normal", "Mon", 2, 23));
-		assertEquals("The swimming pool is close now.", e.getMessage());
-	}
-	@Test
-	void Test_09_For_Coverage() throws Exception {
-		Exception e = assertThrows(Exception.class, ()->P.Cash_check("Normal", "Mon", 2, 10));
-		assertEquals("This age is not alow to enter the swimming pool.", e.getMessage());
-	}
-	@Test
-	void Test_10_For_Coverage() throws Exception {
-		Exception e = assertThrows(Exception.class, ()->P.Cash_check("Normal", "Mon", 76, 10));
-		assertEquals("This age is not alow to enter the swimming pool.", e.getMessage());
-	}
-	@Test
-	void Test_11_For_Coverage() throws Exception {
-		assertEquals(250, P.Cash_check("Normal", "Sun", 40, 6));
-	}
-	@Test
-	void Test_12_For_Coverage() throws Exception {
-		assertEquals(160, P.Cash_check("Normal", "Mon", 11, 8));
-	}
-	@Test
-	void Test_13_For_Coverage() throws Exception {
-		assertEquals(160, P.Cash_check("Normal", "Mon", 60, 8));
-	}
-	***/
+	
+	
 }
